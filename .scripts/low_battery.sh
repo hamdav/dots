a272679232bcf8502392f71059c7b221a2a04876
David Hambraeus <ddhambraeus@gmail.com>
2019-05-05 13:49:15 +0200

    Bettered script to reset bgimage if it doesn't need to charge

--- .scripts/low_battery.sh
+++ .scripts/low_battery.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#Script will change background when battery is low
+# Create a cron job to run this every 5 minutes
+
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 06:15:00 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
     feh --bg-scale ~/Wallpapers/low_battery-wallpaper.jpg
+elif  cat ~/.fehbg | grep -q "/Wallpapers/low_battery-wallpaper.jpg"  ; then
+        feh --bg-scale ~/Wallpapers/wallpaper.jpg
 fi
