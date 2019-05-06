60a4b5abfe9d2ac82d307dcf778dfc4276371be4
David Hambraeus <ddhambraeus@gmail.com>
2019-05-06 17:11:54 +0200

    Percent instead of time

--- .scripts/low_battery.sh
+++ .scripts/low_battery.sh
@@ -4,7 +4,8 @@
 # Create a cron job to run this every 5 minutes
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%"` -lt '12' ]] ; then
     feh --bg-scale ~/Wallpapers/low_battery-wallpaper.jpg
 elif  cat ~/.fehbg | grep -q "/Wallpapers/low_battery-wallpaper.jpg"  ; then
         feh --bg-scale ~/Wallpapers/wallpaper.jpg
