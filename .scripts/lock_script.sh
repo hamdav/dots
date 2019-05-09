8488f290af1c55fd34e71365ce1ad36dae7f5394
David Hambraeus <ddhambraeus@gmail.com>
2019-05-09 09:23:35 +0200

    Lock script

---
+++ .scripts/lock_script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Uses i3lock-color built from AUR
+
+#i3lock --blur=10 --clock --indicator --insidecolor=232C31FF --ringcolor=9EA7A6FF --line-uses-inside --keyhlcolor=2A5491FF --bshlcolor=A03B1EFF --insidevercolor=232C31FF --insidewrongcolor=A03B1Eff --ringvercolor=9EA7A6FF --ringwrongcolor=3F4944FF --separatorcolor=2A5491FF --verifcolor=FFFFFFFF --wrongcolor=232C31FF --timecolor=9EA7A6FF --datecolor=9EA7A6FF
+
+# Alt 
+
+i3lock --blur=10 --clock --indicator \
+    --insidevercolor=FFFFFF00 \
+    --insidewrongcolor=FFFFFF00 \
+    --insidecolor=FFFFFF00 \
+    --ringvercolor=FFFFFF00 \
+    --ringwrongcolor=FFFFFF00 \
+    --ringcolor=FFFFFF00 \
+    --linecolor=FFFFFF00 \
+    --keyhlcolor=8EA01F99 \
+    --bshlcolor=DC571A99 \
+    --separatorcolor=FFFFFF00 \
+    --verifcolor=8EA01FFF \
+    --wrongcolor=DC571AFF \
+    --indpos="w/2:h/2" \
+    --timecolor=C1C1C1FF \
+    --timestr="%H:%M:%S" \
+    --timepos="w/5:2*h/3" \
+    --datecolor=C1C1C1FF \
+    --datestr="%A, %m %Y" \
+    --timesize=90 \
+    --datesize=14 \
+    --verifsize=40 \
+    --wrongsize=40 \
+    --datepos="tx:ty+h/20" \
+    --radius=100 
+
+    
+
+
+    
