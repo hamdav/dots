10adcc0a54541f545032f4a60f8f9ae14d452136
David Hambraeus <ddhambraeus@gmail.com>
2019-08-30 21:11:57 +0200

    Changed wallpaper directory

--- .scripts/low_battery.sh
+++ .scripts/low_battery.sh
@@ -6,7 +6,7 @@
 BATTINFO=`acpi -b`
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%"` -lt '12' ]] ; then
-    feh --bg-scale ~/Wallpapers/low_battery-wallpaper.jpg
+    feh --bg-scale ~/Pictures/Wallpapers/low_battery-wallpaper.jpg
 elif  cat ~/.fehbg | grep -q "/Wallpapers/low_battery-wallpaper.jpg"  ; then
-        feh --bg-scale ~/Wallpapers/wallpaper.jpg
+        feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg
 fi
