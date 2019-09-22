506c6c1e16d685c0ff0728ae5b1456c5648b299d
David Hambraeus <ddhambraeus@gmail.com>
2019-09-22 09:32:33 +0200

    Make the brightness go down when battery is low

--- .scripts/low_battery.sh
+++ .scripts/low_battery.sh
@@ -7,6 +7,7 @@ BATTINFO=`acpi -b`
 
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%"` -lt '12' ]] ; then
     feh --bg-scale ~/Pictures/Wallpapers/low_battery-wallpaper.jpg
+    light -S 0.2
 elif  cat ~/.fehbg | grep -q "/Wallpapers/low_battery-wallpaper.jpg"  ; then
         feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg
 fi
