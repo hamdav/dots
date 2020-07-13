d890f8ce07755ba41a96ba11f155b72349ad5a5c
David Hambraeus <Ddhambraeus@gmail.com>
2020-07-13 13:51:00 +0200

    Remove acpid dep, check /sys/class/.../BAT0 instead

--- .scripts/low_battery.sh
+++ .scripts/low_battery.sh
@@ -3,11 +3,9 @@
 #Script will change background when battery is low
 # Create a cron job to run this every 5 minutes
 
-BATTINFO=`acpi -b`
-
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | cut -f 1 -d "%"` -lt '12' ]] ; then
-    feh --bg-scale ~/Pictures/Wallpapers/low_battery-wallpaper.jpg
+if [[ `cat /sys/class/power_supply/BAT0/status | grep Discharging` && `cat /sys/class/power_supply/BAT0/capacity` -lt '15' ]] ; then
+    feh --bg-scale /home/david/Pictures/Wallpapers/low_battery-wallpaper.jpg
     light -S 0.2
 elif  cat ~/.fehbg | grep -q "/Wallpapers/low_battery-wallpaper.jpg"  ; then
-        feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg
+        feh --bg-scale /home/david/Pictures/Wallpapers/wallpaper.jpg
 fi
